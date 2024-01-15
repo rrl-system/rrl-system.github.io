7f9ec847a1c64f514fec21680287840c49fd84b1
v.a.antoshkin <v.a.antoshkin@mail.ru>
2024-01-15 20:39:29 +0300

    Add hover

--- components/button/link-button.mjs
+++ components/button/link-button.mjs
@@ -19,24 +19,46 @@ customElements.define('link-button', class LinkButton extends BaseElement {
                 :host {
                     display: inline-block;
                     vertical-align: middle;
-                    background-color: var(--background-green);
+                    position: relative;
                     padding: 10px 30px;
                     cursor: pointer;
                 }
-                host(:hover) {
-                    background-color: red;
-                    filter: brightness(2);
+                :host(::before) {
+                    content: '';
+                    position: absolute; /* Фиксируем на одном месте */
+                    left: 0; right: 0; /* Вся ширин */
+                    top: 0; bottom: 0; /* Вся высота */
+                    z-index: -1; /* Фон ниже текста */
+                    /* Параметры фона */
+                    background-color: var(--background-green);
+                })
+                // host(:hover) {
+                //     background-color: blue;
+                //     background: linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)),
+                //     filter: brightness(2);
+                // }
+                // a::before {
+                //     content: '';
+                //     position: absolute; /* Фиксируем на одном месте */
+                //     left: 0; right: 0; /* Вся ширин */
+                //     top: 0; bottom: 0; /* Вся высота */
+                //     z-index: -1; /* Фон ниже текста */
+                //     /* Параметры фона */
+                //     background-color: var(--background-green);
+                // }
+                a::before {
+                    display: inline-block;
+                    content: '';
+                    background-color: tomato;
+                    position: absolute;
+                    z-index: -1;
+                    left: 0; right: 0; /* Вся ширин */
+                    top: 0; bottom: 0; /* Вся высота */
+                }
+                a:hover::before {
+                    filter: brightness(200%);
                 }
 
-                a {
-                    display: inline-block;
-                    text-decoration: none;
-                    text-transform: uppercase;
-                    letter-spacing: 1px;
-                    white-space: nowrap;
-                    font-weight: 600;
-                    color: var(--native-color);
-                }
             `
         ];
     }
@@ -47,9 +69,28 @@ customElements.define('link-button', class LinkButton extends BaseElement {
 
     render() {
         return html`
-            <a href=${this.href || nothing} target=${this.target || nothing} title=${this.title || nothing}>
-                <slot></slot>
-            </a>
+            <style>
+                :host {
+                    background: yellow;
+                }
+                :host(:hover) {
+                    background: linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5));
+                    // filter: brightness(10);
+                }
+                // :host(:before) {
+                //     content: '';
+                //     position: absolute; /* Фиксируем на одном месте */
+                //     left: 0; right: 0; /* Вся ширин */
+                //     top: 0; bottom: 0; /* Вся высота */
+                //     z-index: -1; /* Фон ниже текста */
+                //     /* Параметры фона */
+                //     background-color: var(--background-green);
+                // }
+            </style>
+            <a href="#">Привет</a>
+            // <a href=${this.href || nothing} target=${this.target || nothing} title=${this.title || nothing}>
+            //     <slot></slot>
+            // </a>
         `;
     }
 });
